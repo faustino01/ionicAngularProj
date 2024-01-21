bf91b0227f9ec5156d7a3822fa209edfe0dfd3cb
faustino01 <20181165@uthh.edu.mx>
2024-01-21 11:39:50 -0600

    services

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Preferences } from '@capacitor/preferences';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+}
+
+
+
+
