421a43e9d477e4759df930fac07c7c549edf0395
faustino01 <20181165@uthh.edu.mx>
2024-01-22 23:51:10 -0600

    guardando photos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   constructor(public photoService:PhotoService) {}
+  // ngOnInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+  async ngOnInit(): Promise<void> {
+    await this.photoService.loadSaved();
+  }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
